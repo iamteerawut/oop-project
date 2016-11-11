4f9e1f890afc92ab0c42923d08f424f4463611d2
iamteerawut <t.kitbunjerdjarud@gmail.com>
2016-11-11 19:38:40 +0700

    update for font destination

--- core/src/com/oop/bar/BarGame.java
+++ core/src/com/oop/bar/BarGame.java
@@ -97,7 +97,7 @@ public class BarGame extends ApplicationAdapter{
 		background2 = new Texture("background.png");
 		ready = new TextureRegion(new Texture("ready.png"));
 		gameOver = new TextureRegion(new Texture("gameover.png"));
-		font = new BitmapFont(Gdx.files.internal("howser32.fnt"));
+		font = new BitmapFont(Gdx.files.internal("font/howser-36.fnt"));
 		bar = new TextureRegion(new Texture("bar2.png"));
 		bar2 = new TextureRegion(new Texture("bar.png"));
 		hand_right = new TextureRegion(new Texture("boxB.png"));
